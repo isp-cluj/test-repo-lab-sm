774550ba53230db35222f75813ef3d7f75a7c45b
Mihai H <59961959+automatica-cluj@users.noreply.github.com>
2021-03-27 13:54:54 +0200

    Exemplu 1, test.

--- isp-lab-4-master/src/test/java/isp/lab4/exercise1/TemperatureSensorTest.java
+++ isp-lab-4-master/src/test/java/isp/lab4/exercise1/TemperatureSensorTest.java
@@ -1,5 +1,15 @@
 package isp.lab4.exercise1;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TemperatureSensorTest {
-    //implement minimal tests
+    
+    @Test
+    public void testConstructor(){
+        TemperatureSensor t1 = new TemperatureSensor(10, "Plant_B");
+        assertEquals(10, t1.getValue());
+        assertEquals("Plant_B", t1.getLocation());
+        
+    }
 }
